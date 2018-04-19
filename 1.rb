34941c5fd9041713649d379bd551dcdc18a94fb4
Chethan <crajendra@qwinix.io>
2018-04-19 08:41:09 +0530

    added lines

--- 1.rb
+++ 1.rb
@@ -3,4 +3,28 @@
 # output should be array of hashes with employeeID as key and name as value
 # employeeID should be of format "QT001", "QT002" etc
 # sample output [{"QT001" => "john"}, {"QT002" => "jane"}, ...]
-["john", "jane", "sam", "rockwell", "edward", "brad", "jared"]
+# ["john", "jane", "sam", "rockwell", "edward", "brad", "jared"]
+
+# a=["john", "jane", "sam", "rockwell", "edward", "brad", "jared"]
+#  h={}
+# arr=[]
+#  b= h.merge!(QT001: "john",QT002:"jane",QT003:"sam",QT004:"rockwell",QT005:"edward",QT006:"brad",QT007:"jared")
+#  p b
+#  
+
+# a = [:QT001, :QT002, :QT003, :QT004, :QT005, :QT006, :QT007]
+# b = ["john", "jane", "sam", "rockwell", "edward", "brad", "jared"]
+# arr = a.inject({}) do |x, num|
+#  x[num] = b[a.index(num)]
+#  x
+# end
+# array = []
+# array.push(arr)
+# p array
+
+a = ["john", "jane", "sam", "rockwell", "edward", "brad", "jared"]
+ h = {}
+ array = []
+ b = h.merge!(QT001: "john",QT002:"jane",QT003:"sam",QT004:"rockwell",QT005:"edward",QT006:"brad",QT007:"jared")
+ array.push(b)
+ p array
--- module2.rb
+++ module2.rb
@@ -16,66 +16,151 @@
 #         12
 #         Result is 24
 
-puts "select the option"
-puts "1.addition"
-puts "2.subtraction"
-puts "3.multiplication"
-puts "4.division"
-puts "please select the option"
+# puts "select the option"
+# puts "1.addition"
+# puts "2.subtraction"
+# puts "3.multiplication"
+# puts "4.division"
+# puts "please select the option"
+# $x=gets.to_i
+
+# module Arithmetic
+# 	def add
+# 		if $x==1
+# 		puts "enter the first value"
+# 		@a=Integer(gets)
+# 		puts "Enter the second value"
+# 		@b=Integer(gets)
+# 		@z=@a+@b
+# 		puts "the result is: #{@z}"
+# 	# rescue
+# 	# 	puts "enter valid number"
+# 	elsif 
+# 		end
+	
+# 	end
+# 	def sub
+# 		if $x==2
+# 		puts "enter the first value"
+# 		@a=Integer(gets)
+# 		puts "Enter the second value"
+# 		@b=Integer(gets)
+# 		@z=@a-@b
+# 		puts "the result is: #{@z}"
+# 	# rescue
+# 	# 	puts "enter valid number"
+# 	elsif 
+# 		end	
+# 	end
+	
+# 	def mul
+# 		if $x==3
+# 		puts "enter the first value"
+# 		@a=Integer(gets)
+# 		puts "Enter the second value"
+# 		@b=Integer(gets)
+# 		@z=@a*@b
+# 		puts "the result is: #{@z}"
+# 	# rescue
+# 	# 	puts "enter valid number"
+# 	elsif 
+# 		end	
+# 	end
+
+
+# 	def div
+# 		if $x==4
+# 		puts "enter the first value"
+# 		@a=Integer(gets)
+# 		puts "Enter the second value"
+# 		@b=Integer(gets)
+# 		@z=@a/@b
+# 		puts "the result is: #{@z}"
+# 	# rescue
+# 	# 	puts "enter valid number"
+# 	elsif 
+# 		end	
+# 	end
+
+# class Problems
+# 	extend Arithmetic
+# end
+# puts "****addition*****"
+# Problems.add
+# puts "*****subtractiion***"
+# Problems.sub
+# puts "*****multiplication******"
+# Problems.mul
+# puts "****division*********"
+# Problems.div
+
+puts "please select mathamatics operation"
+puts "1.additon"
+puts "2.substraction"
+puts "3.Multiplication"
+puts "4.Division"
+puts "Please select your option"
 $x=gets.to_i
-
-
-module Arithmetic
+module Arithmetic_operation
 	def add
-		puts "enter the first value"
-		@a=Integer(gets)
-		puts "Enter the second value"
-		@b=Integer(gets)
-		@z=@a+@b
-		puts "the result is: #{@z}"
+	if $x==1
+	begin
+	
+	puts "enter the value 1"
+	@a=Integer(gets)
+	puts "Enter the value 2"
+	@b=Integer(gets)
+	@c=@a+@b
+	puts "the sum of #{@a} and #{@b} value is : #{@c}"
 	rescue
-		puts "enter valid number"
+	puts "enter the valid input"
+	@a=Integer(gets)
+	puts "enter the valid input"
+	@b=Integer(gets)
+	@c=@a + @b
+	puts @c
+	end
+	# rescue => exception e 
+	# p "enter the value input"
+	# end
+	end
 	end
 	def sub
-		puts "enter the first value"
-		@a=Integer(gets)
-		puts "Enter the second value"
-		@b=Integer(gets)
-		@z=@a-@b
-		puts "the result is: #{@z}"
-	rescue
-		puts "enter valid number"
+	if $x==2
+	puts "enter the value 1"
+	@a=Integer(gets)
+	puts "Enter the value 2"
+	@b=Integer(gets)
+	@c=@a-@b
+	puts "the substraction of #{@a} and #{@b} value is : #{@c}"
+
+	end
 	end
 	def mul
-		puts "enter the first value"
-		@a=Integer(gets)
-		puts "Enter the second value"
-		@b=Integer(gets)
-		@z=@a*@b
-		puts "the result is: #{@z}"
-	rescue
-		puts "enter valid number"
+	if $x==3
+	puts "enter the value 1"
+	@a=Integer(gets)
+	puts "Enter the value 2"
+	@b=Integer(gets)
+	@c=@a*@b
+	puts "the multiplication of #{@a} and #{@b} value is : #{@c}"
 	end
-
+	end
 	def div
-		puts "enter the first value"
-		@a=Integer(gets)
-		puts "Enter the second value"
-		@b=Integer(gets)
-		@z=@a/@b
-		puts "the result is: #{@z}"
-	rescue
-		puts "enter valid number"
+	if $x==4
+	puts "enter the value 1"
+	@a=Integer(gets)
+	puts "Enter the value 2"
+	@b=Integer(gets)
+	@c=@a/@b
+	puts "the Division of #{@a} and #{@b} value is : #{@c}"
+	end
 	end
 end
-class Problems
-	extend Arithmetic
+class All
+	extend Arithmetic_operation
 end
-puts "****addition*****"
-Problems.add
-puts "*****subtractiion***"
-Problems.sub
-puts "*****multiplication******"
-Problems.mul
-puts "****division*********"
-Problems.div
+All.add
+All.sub
+All.mul
+All.div
